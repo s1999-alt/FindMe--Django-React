35183be5c2363fd3833ab3ec3e07771223072688
Siyad Savad <siyadsavad313@gmail.com>
2024-04-15 10:40:46 +0530

    asgi file changed

--- backend/FindMe/asgi.py
+++ backend/FindMe/asgi.py
@@ -8,12 +8,14 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 """
 
 import os
-
+import django
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'FindMe.settings')
+django.setup()
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from users.route import websocket_urlpattern
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'FindMe.settings')
+
 
 application = get_asgi_application()
 
--- backend/FindMe/settings.py
+++ backend/FindMe/settings.py
@@ -10,7 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 import os
-import django
 from pathlib import Path
 import environ
 from datetime import timedelta
@@ -22,17 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 dotenv_path = BASE_DIR / '.env'
 environ.Env.read_env(BASE_DIR / ".env")
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'FindMe.settings')
-
-
-from django.conf import settings
-settings.configure()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = settings.SECRET_KEY
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEBUG")
