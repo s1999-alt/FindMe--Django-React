8327a52f276c4ead2cbfa1996d1a8b21b70fb51a
Siyad Savad <siyadsavad313@gmail.com>
2024-04-17 08:57:17 +0530

    the STATIC_ROOT = BASE_DIR / 'static' changed ,give CSRF_COOKIE_SECURE = True , to solve the django default admin problem

--- backend/FindMe/settings.py
+++ backend/FindMe/settings.py
@@ -152,8 +152,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR / 'static'
 STATIC_URL = 'static/'
+
+CSRF_COOKIE_SECURE = True
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
